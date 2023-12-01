fabce6815254f81994a08b0ab5b438900f1de59b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 14:19:40 -0500

    if unauth redirect to public page

--- study-sphere-front-end/src/app/auth.guard.ts
+++ study-sphere-front-end/src/app/auth.guard.ts
@@ -1,10 +1,11 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   if (inject(AuthService).session) {
     return true;
   }
+  inject(Router).navigateByUrl('/');
   return false;
 };
