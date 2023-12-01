f77079476ba868d39844344aa590ea5251f2b615
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 09:04:24 -0500

    lazy loading public module landing

--- study-sphere-front-end/src/app/public/public-routing.module.ts
+++ study-sphere-front-end/src/app/public/public-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { PublicComponent } from './public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PublicComponent, children: [
+      { path: '', component: LandingComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
