ef7533e7aebee67be0b9903e13ba320542416368
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 10:09:26 -0500

    basic set up for login page

--- study-sphere-front-end/src/app/login/login.component.ts
+++ study-sphere-front-end/src/app/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent {
+
+// }
+// login.component.ts
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'] // You can apply your CSS styles here
 })
 export class LoginComponent {
 
+  @ViewChild('passwordToggle') passwordToggle!: ElementRef;
+  isPasswordVisible: boolean = false;
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    if (this.isPasswordVisible) {
+      passwordInput.type = 'text';
+      this.passwordToggle.nativeElement.textContent = 'Hide Password';
+    } else {
+      passwordInput.type = 'password';
+      this.passwordToggle.nativeElement.textContent = 'Show Password';
+    }
+  }
 }
