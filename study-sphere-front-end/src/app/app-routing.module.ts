0be089f98906bb7e79e3a236920ee91e29567388
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 14:14:27 -0500

    can't access by typing /admin

--- study-sphere-front-end/src/app/app-routing.module.ts
+++ study-sphere-front-end/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then((m) => m.PublicModule), },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule), },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule), canActivate: [authGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
--- study-sphere-front-end/src/app/auth.guard.ts
+++ study-sphere-front-end/src/app/auth.guard.ts
@@ -1,5 +1,10 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  if (inject(AuthService).session) {
+    return true;
+  }
+  return false;
 };
