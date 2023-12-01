ad1d615b467b75426003d8beec596b27861e54f3
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 11:02:17 -0500

    save valid userinfo into local

--- study-sphere-front-end/src/app/auth.service.ts
+++ study-sphere-front-end/src/app/auth.service.ts
@@ -13,5 +13,17 @@ export class AuthService {
       password: '12345'
     }
   ]
+  session: any;
   constructor() { }
+
+  login(username: string, password: string) {
+    let user = this.users.find((u) => u.username === username && u.password === password);
+    if (user) {
+      //save the user into session
+      this.session = user;
+      // refresh and save the session into local
+      localStorage.setItem('session', JSON.stringify(this.session));
+    }
+    return user;
+  }
 }
