3133ee1153042ced45edcd40eeaadbc0f2998ebb
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-01 13:58:39 -0500

    one way to redirect user to the home page if click logout button

--- study-sphere-front-end/src/app/admin/admin-routing.module.ts
+++ study-sphere-front-end/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { UsersPageComponent } from './users-page/users-page.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  {
+    path: '', component: AdminComponent, children: [
+      { path: '', component: UsersPageComponent }
+    ]
+  }
 ];
 
 @NgModule({
