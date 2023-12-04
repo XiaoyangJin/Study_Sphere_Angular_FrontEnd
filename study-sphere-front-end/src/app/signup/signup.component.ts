13a3d9ee8fcc27b02757f32585d300397e302d07
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-04 17:48:35 -0500

    design signup function

--- study-sphere-front-end/src/app/signup/signup.component.ts
+++ study-sphere-front-end/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  signup() { }
+
+  form: FormGroup = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) { }
+
+  signup() {
+    //put it into the database(postgres sql)[after backend nodejs?]
+    //after email authroization(future optional)
+    //change the authority to be able to login
+    //redirect to the public page
+    this.router.navigate(['']);
+  }
 }
