bb1644db42b9b85906251e24b09c002eef0191e8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-06 09:09:54 -0500

    basic post detail design

--- study-sphere-front-end/src/app/app-routing.module.ts
+++ study-sphere-front-end/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { authGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then((m) => m.PublicModule), },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule), canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'playground', component: PlaygroundComponent }
+  { path: 'playground', component: PlaygroundComponent },
+  { path: 'post', component: PostComponent }
 ];
 
 @NgModule({
