100042fe4a1d1df50bd97754f46c5707bdbddcfc
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-05 19:36:42 -0500

    basic filter function

--- study-sphere-front-end/src/app/playground/playground.component.ts
+++ study-sphere-front-end/src/app/playground/playground.component.ts
@@ -1,3 +1,4 @@
+// playground.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent {
+  posts: any[] = [
+    { title: 'Post 1 Title', summary: 'This is a summary of Post 1.', keywords: ['keyword1'] },
+    { title: 'Post 2 Title', summary: 'This is a summary of Post 2.', keywords: ['keyword2'] },
+    { title: 'Post 3 Title', summary: 'This is a summary of Post 3.', keywords: ['keyword1', 'keyword3'] },
+  ];
 
+  filteredPosts: any[] = [];
+
+  filterByKeyword(keyword: string) {
+    this.filteredPosts = this.posts.filter(post => post.keywords.includes(keyword));
+  }
 }
