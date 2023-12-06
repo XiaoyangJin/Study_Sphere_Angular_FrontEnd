c4ce1cbc03ebb7a434e1efb42692e2a7c1b46364
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-05 19:05:26 -0500

    route to pg

--- study-sphere-front-end/src/app/app-routing.module.ts
+++ study-sphere-front-end/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { PlaygroundComponent } from './playground/playground.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then((m) => m.PublicModule), },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule), canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'playground', component: PlaygroundComponent }
 ];
 
 @NgModule({
