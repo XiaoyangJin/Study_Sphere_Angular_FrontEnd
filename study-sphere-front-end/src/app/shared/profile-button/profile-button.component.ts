4bf31a1df51d9592ae092600a7c749675793a4af
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-15 10:29:14 -0500

    basic setup for profile button

--- study-sphere-front-end/src/app/shared/profile-button/profile-button.component.ts
+++ study-sphere-front-end/src/app/shared/profile-button/profile-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-button',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-button.component.css']
 })
 export class ProfileButtonComponent {
+  constructor(private router: Router) { }
 
+  navigateToProfile() {
+    this.router.navigate(['/admin']);
+  }
 }
