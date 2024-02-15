0fbee3255f31fb151aebc93d5571c2f5774835e4
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-14 22:03:02 -0500

    solve createpostcomponent http error

--- study-sphere-front-end/src/app/createPost/create-post.component.spec.ts
+++ study-sphere-front-end/src/app/createPost/create-post.component.spec.ts
@@ -2,15 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreatePostComponent } from './create-post.component';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PostService } from '../post.service';
 
 describe('CreatePostComponent', () => {
   let component: CreatePostComponent;
   let fixture: ComponentFixture<CreatePostComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CreatePostComponent],
+      imports: [HttpClientTestingModule], // Include HttpClientTestingModule here
+      providers: [PostService] // Provide PostService
+    })
+      .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CreatePostComponent]
-    });
     fixture = TestBed.createComponent(CreatePostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- study-sphere-front-end/src/app/playground/playground.component.spec.ts
+++ study-sphere-front-end/src/app/playground/playground.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlaygroundComponent } from './playground.component';
 
+
 describe('PlaygroundComponent', () => {
   let component: PlaygroundComponent;
   let fixture: ComponentFixture<PlaygroundComponent>;
