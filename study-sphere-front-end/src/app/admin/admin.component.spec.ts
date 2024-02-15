80119183edd9aee8fdb7ef8e3210912e5c18fb02
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-15 08:58:07 -0500

    bypass router-outlet test in admin.component.spec.ts

--- study-sphere-front-end/src/app/admin/admin.component.spec.ts
+++ study-sphere-front-end/src/app/admin/admin.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AdminComponent]
+      declarations: [AdminComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
