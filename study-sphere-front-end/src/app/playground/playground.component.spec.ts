46b8b32749107a0da6126d8bcc68154fe6e11036
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-15 08:55:28 -0500

    bypass logoutbar in palyground component

--- study-sphere-front-end/src/app/playground/playground.component.spec.ts
+++ study-sphere-front-end/src/app/playground/playground.component.spec.ts
@@ -4,10 +4,26 @@ import { PlaygroundComponent } from './playground.component';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PostService } from '../post.service';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-bar',
+  template: ''
+})
+class MockLogoutBarComponent { }
 
 describe('PlaygroundComponent', () => {
   let component: PlaygroundComponent;
   let fixture: ComponentFixture<PlaygroundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MockLogoutBarComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    })
+      .compileComponents();
+  });
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
