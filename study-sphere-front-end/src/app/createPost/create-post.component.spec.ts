ab057f140c5feab71b9b58c1a86269126cddb0e0
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-14 21:54:28 -0500

    solve post service http error

--- study-sphere-front-end/src/app/createPost/create-post.component.spec.ts
+++ study-sphere-front-end/src/app/createPost/create-post.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreatePostComponent } from './create-post.component';
 
+
 describe('CreatePostComponent', () => {
   let component: CreatePostComponent;
   let fixture: ComponentFixture<CreatePostComponent>;
--- study-sphere-front-end/src/app/post.service.spec.ts
+++ study-sphere-front-end/src/app/post.service.spec.ts
@@ -2,15 +2,21 @@ import { TestBed } from '@angular/core/testing';
 
 import { PostService } from './post.service';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('PostService', () => {
   let service: PostService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Include HttpClientTestingModule
+      providers: [PostService] // Provide your service
+    });
     service = TestBed.inject(PostService);
   });
 
   it('should be created', () => {
+    const service: PostService = TestBed.inject(PostService);
     expect(service).toBeTruthy();
   });
 });
