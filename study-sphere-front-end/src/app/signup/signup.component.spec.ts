a8331a870fe3a7b6e654360c89580fea666d9268
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-16 08:58:09 -0500

    import new type of elements in signup component test

--- study-sphere-front-end/src/app/signup/signup.component.spec.ts
+++ study-sphere-front-end/src/app/signup/signup.component.spec.ts
@@ -4,9 +4,16 @@ import { SignupComponent } from './signup.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 
+//import for customed tests
+import { BrowserModule, By } from '@angular/platform-browser'; // allow us to select elements from the DOM
+import { DebugElement } from '@angular/core';
+
+
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
