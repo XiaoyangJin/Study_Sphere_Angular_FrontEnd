570af1349d545b7d43f80fb5c17e8b0921e6226c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-02-21 19:38:55 -0500

    add navigation test for create button

--- study-sphere-front-end/src/app/shared/create-button/create-button.component.spec.ts
+++ study-sphere-front-end/src/app/shared/create-button/create-button.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateButtonComponent } from './create-button.component';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CreateButtonComponent', () => {
   let component: CreateButtonComponent;
   let fixture: ComponentFixture<CreateButtonComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CreateButtonComponent]
+      declarations: [
+        CreateButtonComponent
+      ],
+      imports: [
+        RouterTestingModule
+      ]
     });
     fixture = TestBed.createComponent(CreateButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // navigate to '/create-post' page
+  it('should redirect to create post page', () => {
+    spyOn(router, 'navigate');
+    component.navigateToCreatePost();
+    expect(router.navigate).toHaveBeenCalledWith(['/create-post']);
+  })
 });
