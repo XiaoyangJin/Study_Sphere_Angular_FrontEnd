569b9aef9637af29b67f6dff4ba18d8cc73be66e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-01 14:19:56 -0500

    try to implement fetch tests but face a bug

--- study-sphere-front-end/src/app/playground/playground.component.spec.ts
+++ study-sphere-front-end/src/app/playground/playground.component.spec.ts
@@ -6,20 +6,45 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PostService } from '../post.service';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-logout-bar',
   template: ''
 })
 class MockLogoutBarComponent { }
 
+class MockPostService {
+  fetchPosts = jasmine.createSpy('fetchPosts');
+  postsUpdated = of([{ id: 1, title: 'Test Post', summary: 'Test Summary', main_content: 'This is a test' }]);
+
+  getPostById = jasmine.createSpy('getPostById').and.returnValue({ id: 1, title: 'Test Post', summary: 'Test Summary', main_content: 'This is a test' });
+}
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
+
 describe('PlaygroundComponent', () => {
   let component: PlaygroundComponent;
   let fixture: ComponentFixture<PlaygroundComponent>;
+  let mockPostService: MockPostService;
+  let mockRouter: MockRouter;
 
   beforeEach(async () => {
+    mockPostService = new MockPostService();
+    mockRouter = new MockRouter();
+
     await TestBed.configureTestingModule({
-      declarations: [MockLogoutBarComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      declarations: [MockLogoutBarComponent, PlaygroundComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: PostService, useValue: mockPostService },
+        { provide: Router, useValue: mockRouter },
+        ChangeDetectorRef
+      ]
     })
       .compileComponents();
   });
@@ -43,4 +68,12 @@ describe('PlaygroundComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should call fetchPosts and subscribe to postsUpdated on ngOnInit', () => {
+  //   // ngOnInit is called by fixture.detectChanges() in the beforeEach block
+  //   fixture.detectChanges();
+  //   expect(mockPostService.fetchPosts).toHaveBeenCalled();
+  //   expect(component.posts.length).toBeGreaterThan(0);
+  // });
+
 });
